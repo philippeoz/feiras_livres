6a06cfc9f48f288aaa9990afc640f354274af26a
Philippe <philip_will15@hotmail.com>
2017-09-12 02:38:01 -0300

    Adicionando Model FeirasLivres

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class FeiraLivre(models.Model):
+    """
+    Model FeiraLivre
+    Define os atributos de uma feira livre
+    """
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(null=True, blank=True)
+    nível_agregação = models.CharField(max_length=255, null=True, blank=True)
+    responsavel = models.CharField(max_length=255, null=True, blank=True)
+    data_referencia = models.DateField(null=True, blank=True)
+    sigilo = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
